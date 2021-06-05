dc7b7bdb19ee338908fc3906d37343e859a57579
Jamie Evawin <iamjevawin@gmail.com>
2021-06-05 14:48:40 +0100

    Images stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,21 @@
-const pluginTailwindCSS = require("eleventy-plugin-tailwindcss");
-const markdownIt = require("markdown-it");
-const markdownItClass = require("markdown-it-class");
 const htmlmin = require("html-minifier");
+const Image = require("@11ty/eleventy-img");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const markdownIt = require("@gerhobbelt/markdown-it");
+const markdownItClass = require("markdown-it-class");
+const nunjucks = require("nunjucks");
+const pluginTailwindCSS = require("eleventy-plugin-tailwindcss");
 const posthtml = require("posthtml");
 const uglify = require("posthtml-minify-classnames");
+require("dotenv").config();
+
+// Environment variables in Nunjucks
+nunjucks.configure("views", {}).addGlobal("CFWA_TOKEN", process.env.CFWA_TOKEN);
+
+// eleventy input and output dirs
+const inputDir = "_src/assets";
+const outputDir = "_dist";
 
 // markdown-it-class mapping
 //const mapping = require("./_src/assets/styles/markdown.js");
@@ -44,7 +56,9 @@ const mapping = {
   strong: ["font-bold"],
 };
 
-const md = markdownIt({ linkify: true, html: true });
+// enable markdown-it formatting
+const md = markdownIt({ linkify: true, html: true, typographer: true });
+// tell markdown-it to use markdown-it-class plugin
 md.use(markdownItClass, mapping);
 
 module.exports = (config) => {
@@ -58,18 +72,81 @@ module.exports = (config) => {
     src: "_src/assets/styles/my.css",
   });
 
-  // markdown-it-class
+  // markdown-it, classnames and responsive images
   config.setLibrary("md", md);
 
+  // responsive images
+  config.addTransform("responsiveimg", async (content, outputPath) => {
+    // eleventy-image function
+    async function eleventyImg(image) {
+      // get the `src` and `alt` of the image element
+      let src = image.getAttribute("src");
+
+      if (src === undefined) {
+        // no src = no chance
+        throw new Error(`Missing \`src\` in eleventyImg!`);
+      }
+
+      if (src.slice(0, 1) === "/") {
+        // correct directory for local images
+        src = `${__dirname}/${inputDir}${src}`;
+      }
+
+      // run image through elevnty-img
+      // let metadata = await Image(src, {
+      //   widths: [600],
+      //   formats: ["jpeg"],
+      // });
+
+      // let data = metadata.jpeg[metadata.jpeg.length - 1];
+
+      // replace image details with new image details
+      image.setAttribute("loading", "lazy");
+      image.setAttribute("decoding", "async");
+      // image.setAttribute("width", data.width);
+      // image.setAttribute("height", data.height);
+      // image.setAttribute("src", data.src);
+
+      // send the image back
+      return image;
+    }
+
+    // only apply transforms if the output is html (not xml or css or something)
+    if (outputPath.endsWith(".html")) {
+      // feed the content into jsdom
+      const dom = new JSDOM(content);
+      const document = dom.window.document;
+
+      // find the image elements via `queryselectorall`, replace this selector with your own custom one
+      const imageElems = document.querySelectorAll("img");
+
+      // no images? crack on
+      if (imageElems.length === 0) {
+        return content;
+      }
+
+      // images? let's responsivise them
+      imageElems.forEach(image => {
+        image.setAttribute("src", null);
+        //image = eleventyImg(image);
+      });
+
+      return `<!DOCTYPE html> ${document.documentElement.outerHTML}`;
+    }
+
+    return content;
+  });
+
   // minify html and uglify classnames
+  // TODO production only
   config.addTransform("htmlmin", async (content, outputPath) => {
-    if(outputPath.endsWith(".html")) {
-      const {html} = await posthtml().use(uglify()).process(content);
+    if (outputPath.endsWith(".html")) {
+      const { html } = await posthtml().use(uglify()).process(content);
 
       let minified = htmlmin.minify(html, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
       });
 
       return minified;
@@ -78,13 +155,11 @@ module.exports = (config) => {
     return content;
   });
 
-  // passthrough images
-  config.addPassthroughCopy("_src/assets/images/uploads");
-
   // set output dir
   return {
     dir: {
-      output: "_dist",
+      input: inputDir,
+      output: outputDir,
     },
   };
 };
